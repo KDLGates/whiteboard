755c603c3c6945c1e9e5f23084bde8a22a041e26
Kris Gates <kdlgates@kdlgates.com>
2017-04-19 13:14:32 -0400

    Pulled PyDbLite, presumably it would be more trouble than it's worth rather than just using Flask users/sessions.

--- whiteboard/whiteboard.py
+++ whiteboard/whiteboard.py
@@ -11,11 +11,14 @@ import sqlite3
 import tempfile
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
-from pydblite import Base
 
 #config file start
 app = Flask(__name__) # create the application instance :)
 app.config.from_object(__name__) # load config from this file , whiteboard.py
+
+# Flask's debug parameter automatically monitors for code changes
+# (exchanges performance for not having to restart the app for changes)
+app.run(debug = True)
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
@@ -24,14 +27,6 @@ app.config.update(dict(
     USERNAME='admin',
     PASSWORD='default'
 ))
-
-# Instantiation of user database for pyDbLite
-databasePath = tempfile.gettempdir()
-os.chdir(databasePath)
-users = Base(databasePath + '\whiteboard.pdl')
-# If the db already exists, the create function will automatically just open it.
-users.create('username', 'password', mode='open')
-
 
 app.config.from_envvar('WHITEBOARD_SETTINGS', silent=True)
 #config file end
@@ -87,16 +82,17 @@ def add_entry():
     if not session.get('logged_in'):
         abort(401)
     db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
-    db.commit()
-    flash('New entry was successfully posted')
+    if len(request.form['text']) != 0:
+        db.execute('insert into entries (title, text) values (?, ?)',
+                     [request.form['title'], request.form['text']])
+        db.commit()
+        flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    # Populate a dictionary of existing users.
-    usersDict = [user for user in users]
+    # TODO: Execute a query to populate a dictionary of existing users.
+    usersDict = {}
     # If there isn't already a user present with that user name...
     if request.form['username'] not in usersDict:
         # TODO: Insert record for new user and return confirmation page.
@@ -105,6 +101,7 @@ def register():
     else:
         # TODO: Return an error and return the user to the registration page.
         pass
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
